b1a32c6d2dd37c0b30633e5eb6c0af003b1468f8
Robert Volkmann <robert.volkmann@x-cellent.com>
2024-05-14 16:40:39 +0200

    Remove hardcoded cluster ip 10.2.10.2 for the virtual garden

--- pkg/operator/controller/garden/garden/components.go
+++ pkg/operator/controller/garden/garden/components.go
@@ -9,7 +9,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
-	"os"
 	"strings"
 	"time"
 
@@ -511,11 +510,6 @@ func (r *Reconciler) newKubeAPIServerService(log logr.Logger, garden *operatorv1
 		annotations = settings.LoadBalancerServices.Annotations
 	}
 
-	var clusterIP string
-	if os.Getenv("GARDENER_OPERATOR_LOCAL") == "true" {
-		clusterIP = "10.2.10.2"
-	}
-
 	return kubeapiserverexposure.NewService(
 		log,
 		r.RuntimeClientSet.Client(),
@@ -532,7 +526,6 @@ func (r *Reconciler) newKubeAPIServerService(log logr.Logger, garden *operatorv1
 		nil,
 		nil,
 		nil,
-		clusterIP,
 	), nil
 }
 
